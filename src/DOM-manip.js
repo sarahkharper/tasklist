3a2a6a5940518760c562601f7a5cd19b293bb236
sarahkharper <skhaao@gmail.com>
2025-01-23 10:05:07 -0800

    Tidy up sidebar UI and add task form open close functionality

---
+++ src/DOM-manip.js
@@ -0,0 +1,16 @@
+
+//event listener to toggle 'show' class for form expand buttons
+
+export function toggleShow(elem, type){
+    if (type === "open"){
+        var formContainer = elem.nextElementSibling;
+    } else if (type === "close"){
+        var formContainer = elem.closest(".form-box");
+    }
+    const formRef = formContainer.querySelector("form");
+
+    elem.addEventListener("click", () => {
+        formContainer.classList.toggle("show");
+        formRef.reset();
+    })
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import "./styles.css";
 import "../node_modules/modern-normalize/modern-normalize.css";
 import "./fa-icons/css/all.css";
 import {createTodo, greeting} from "./todo.js";
+import * as domManip from "./DOM-manip.js";
 
 const todoList = [];
 
 const test = createTodo('Test', 'High', 'Now', 'Home', 'Testing');
+
+const newTaskBtn = document.querySelector(".form-expand-btn");
+const closeBtn = document.querySelector(".close-btn");
+domManip.toggleShow(newTaskBtn, "open");
+domManip.toggleShow(closeBtn, "close");
