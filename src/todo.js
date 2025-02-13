55b6bebe77017b1c18aebf71a7f9e809e6a19fd8
sarahkharper <skhaao@gmail.com>
2025-02-12 16:01:56 -0800

    Added JSON conversion functions for todo objects

--- src/todo.js
+++ src/todo.js
@@ -24,11 +24,11 @@ export function createTodo (todo){
 
     //format deadline date
     if(todo.deadline){
-        console.log(todo.deadline);
+        //console.log(todo.deadline);
         const rawDate = new Date(todo.deadline.replace(/-/g, '\/').replace(/T.+/, ''));
-        console.log(rawDate);
+        //console.log(rawDate);
         const formattedDate = datefns.format(rawDate, 'MM-dd-yyyy');
-        console.log(formattedDate);
+        //console.log(formattedDate);
         todo.deadline = formattedDate;
     }
 
@@ -38,8 +38,42 @@ export function createTodo (todo){
     };
 
     todo.getState = () => isComplete; //function to get completion status
-    
+
     return todo;
+};
+
+export function convertTodotoJson(todo){
+    const jsonTodo = {...todo, uuid: todo.getUUID(), isComplete: todo.getState(), type: "todo"};
+    return JSON.stringify(jsonTodo);
+}
+
+export function convertJsontoTodo(jsonTodo){
+    const todo = JSON.parse(jsonTodo);
+
+    const uuid = todo.uuid;
+    const isComplete = todo.isComplete;
+
+    //add function to access uuid
+    todo.getUUID = () => {
+        return uuid;
+    }
+
+    //add function to get type
+    todo.getType = () => {
+        return "todo";
+    }
+
+    todo.changeComplete = () => { //function to change completion status
+        isComplete = !isComplete
+        return isComplete;
+    };
+
+    todo.getState = () => isComplete; //function to get completion status
+
+    //delete properites that should be private
+    delete todo.uuid;
+    delete todo.isComplete;
+    delete todo.type;
 };
 
 export function addObjToArray(arrayName, objName){
@@ -54,9 +88,9 @@ export function submitForm(form, type, arrayName){
         } else if(type === "project"){
             var todo = projFromForm(form);
         }
-        console.log(todo);
+        //console.log(todo);
         addObjToArray(arrayName, todo);
-        console.log(arrayName);
+        //console.log(arrayName);
         updateUI(arrayName);
     })
 }
@@ -64,16 +98,16 @@ export function submitForm(form, type, arrayName){
 export function todoFromForm(form){
     //add submit event listener to get todo data from form
         const todoData = objFromForm(form);
-        console.log(todoData);
+        //console.log(todoData);
         const todo = createTodo(todoData);
         return todo;
 }
 
 export function objFromForm(form){
     const formData = new FormData(form);
-    console.log(formData);
+    //console.log(formData);
     const obj = Object.fromEntries(formData);
-    console.log(obj);
+    //console.log(obj);
     return obj;
 }
 
